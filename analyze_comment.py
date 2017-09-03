dae65943a638625c7193032bf1bf5cc68aabef82
cleland <clelandgt@163.com>
2017-09-03 16:02:12 +0800

    convert tab to  blank space of indentation

--- analyze_comment.py
+++ analyze_comment.py
@@ -16,34 +16,34 @@ MAX_WORDS = 500
 
 
 def get_words(path):
-	with open(path, 'r') as f:
-		content = f.read()
-	return analyse.textrank(content,topK=MAX_WORDS,withWeight=True)
+    with open(path, 'r') as f:
+        content = f.read()
+    return analyse.textrank(content,topK=MAX_WORDS,withWeight=True)
 
 
 def draw_wordcloud(words):
-	img_path = os.path.join(BASE_DIR, 'zhanlang.png')
-	font_path = os.path.join(BASE_DIR, 'simsun.ttc')
-	back_coloring = imread(img_path)
-	wordcloud = WordCloud(
-		font_path=font_path,
-	    background_color="white",
-	    max_words=MAX_WORDS,
-	    mask=back_coloring,
-	    max_font_size=80,
-	    random_state=42
-	) 
-	wordcloud.generate_from_frequencies(dict(words))
-	wordcloud_img_path = os.path.join(BASE_DIR, 'zhanlang_word_cloud.png')
-	wordcloud.to_file(wordcloud_img_path)
+    img_path = os.path.join(BASE_DIR, 'zhanlang.png')
+    font_path = os.path.join(BASE_DIR, 'simsun.ttc')
+    back_coloring = imread(img_path)
+    wordcloud = WordCloud(
+        font_path=font_path,
+        background_color="white",
+        max_words=MAX_WORDS,
+        mask=back_coloring,
+        max_font_size=80,
+        random_state=42
+    ) 
+    wordcloud.generate_from_frequencies(dict(words))
+    wordcloud_img_path = os.path.join(BASE_DIR, 'zhanlang_word_cloud.png')
+    wordcloud.to_file(wordcloud_img_path)
 
 
 def main():
-	text_path = os.path.join(BASE_DIR, 'zhanlang_comments')
-	words = get_words(text_path)
-	draw_wordcloud(words)
-	
+    text_path = os.path.join(BASE_DIR, 'zhanlang_comments')
+    words = get_words(text_path)
+    draw_wordcloud(words)
+    
 
 if __name__ == '__main__':
-	main()
+    main()
 
